015b49918376668ad0f3b1dc210796dc3a5ddb6f
Jade Burgin <jjr1945@rit.edu>
2023-03-06 09:44:06 -0500

    add tokenize function

--- nlp_utils.py
+++ nlp_utils.py
@@ -3,6 +3,17 @@ This module contains utility functions for natural language processing
 
 """
 from nltk.corpus import stopwords
+from textblob import TextBlob
 
-def tokenize_words():
-    return 
+def tokenize_words(str):
+    """
+    tokenize words with textblob
+    """
+    tokenized_words = []
+    blob = TextBlob(str)
+    useless_words = stopwords.words("english") + "" + " " + "\n"
+    for word in str:
+        if str not in useless_words:
+            tokenized_words.append(str)
+
+    return tokenized_words
