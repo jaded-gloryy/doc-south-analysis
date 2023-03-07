04fde18a7f4f6a6cf46bcb170a96f9d4545bd110
Jade.B <jjr1945@rit.edu>
2023-03-07 11:08:13 -0500

    update to use TextBlob library

--- nlp_utils.py
+++ nlp_utils.py
@@ -2,18 +2,24 @@
 This module contains utility functions for natural language processing
 
 """
+# import nltk
+# nltk.download('stopwords')
+# nltk.download('punkt')
 from nltk.corpus import stopwords
 from textblob import TextBlob
+
+
 
 def tokenize_words(str):
     """
     tokenize words with textblob
     """
     tokenized_words = []
-    blob = TextBlob(str)
-    useless_words = stopwords.words("english") + "" + " " + "\n"
-    for word in str:
-        if str not in useless_words:
-            tokenized_words.append(str)
+    blob = TextBlob(str).words
+    useless_words = stopwords.words("english") + ["" , " ", "\n"]
+    for word in blob:
+        if word not in useless_words:
+            tokenized_words.append(word)
 
     return tokenized_words
+
